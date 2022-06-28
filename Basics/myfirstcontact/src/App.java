9f72f6ed059bd27b13d4e5dde1f4d648161c1647
CarlosJunioor <carlosjunior866@gmail.com>
2022-06-28 16:15:17 +0100

    java

--- Basics/myfirstcontact/src/App.java
+++ Basics/myfirstcontact/src/App.java
@@ -4,17 +4,17 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        Scanner teclado=new Scanner(System.in);
-        
-        int x=0, y=0;
+        try (Scanner teclado = new Scanner(System.in)) {
+            int x=0, y=0;
 
-        System.out.println("Digite o 1 Numero: ");
-        x=teclado.nextInt();
+            System.out.println("Digite o 1 Numero: ");
+            x=teclado.nextInt();
 
-        System.out.println("Digite o 2 numero: ");
-        y=teclado.nextInt();
+            System.out.println("Digite o 2 numero: ");
+            y=teclado.nextInt();
 
-        System.out.println("A soma dos números é "+(x+y));
+            System.out.println("A soma dos números é "+(x+y));
+        }
         
     }
 }
--- Basics/myfirstcontact/src/app1.java
+++ Basics/myfirstcontact/src/app1.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class app1 {
     public static void main(String[] args) throws Exception {
-    System.out.println("Hello World!"); 
+       System.out.println("Qual sera o exercicio?");
+
+       Scanner scanner = new Scanner (System.in);
+       int var = 0;
+       String input = scanner.next();
+       int answer = Integer.parseInt(input);
+
+       do{
+        input = scanner.next();
+        answer = Integer.parseInt(input);
+        var = answer;
+
+    switch (var) {
+        case 1:
+        System.out.println("Exercicio 1");
+        break;
+
+        case 2:
+        System.out.print("Exercicio 2");
+        break;
     }
-}
+    } while (var==1 || var == 2);}}
+
